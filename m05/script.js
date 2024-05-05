9faf398c35d6b131b44c9e1a0b41e6089f855bfc
trevortsai <108424502+trevortsai@users.noreply.github.com>
2024-05-04 23:51:08 -0700

    Update script.js

--- m05/script.js
+++ m05/script.js
@@ -1,7 +1,7 @@
 var table; // Unit of table
 table = prompt('Enter number for table:');
 var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var msg = '<h2>Multiplication Table</h2>';              // Message
 
 while (i < 11) {
   msg += (i + ' * ' + table + ' = ') + (i * table) + '<br />';
