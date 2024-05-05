72bdd708ec2739ba4b8d77ea373fbb4710cc88d9
trevortsai <108424502+trevortsai@users.noreply.github.com>
2024-05-04 23:41:03 -0700

    Update script.js

--- m05/script.js
+++ m05/script.js
@@ -1,15 +1,11 @@
 var table; // Unit of table
 table = prompt('Enter number for table:');
-var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '';              // Message
 
-if (operator === 'multiplication') {
-  // Do addition
-  while (i < 11) {
-    msg += (i + ' * ' + table + ' = ') + (i * table) + '<br />';
-    i++;
-  }
+while (i < 11) {
+  msg += (i + ' * ' + table + ' = ') + (i * table) + '<br />';
+  i++;
 }
 // Write the message into the page
 var el = document.getElementById('blackboard');
