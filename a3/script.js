b5850fdcdcbde896eb0fc46d69fa231a1e7d58e8
trevortsai <108424502+trevortsai@users.noreply.github.com>
2024-05-12 00:46:13 -0700

    Create script.js

---
+++ a3/script.js
@@ -0,0 +1,61 @@
+function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
+upRight(pHeight, pColorEven, pColorOdd, pSymbol);
+downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+
+}
+
+function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=0;i<pHeight;i++){
+rLine +="<p>";
+//Create each line on the Rhombus
+for(k=0;k<(pHeight-i); k++) {
+  rLine +="<span style='color:#FFFFFF;'>" + pSymbol +"</span>";
+}
+for(j=0;j<=i*2;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("upRight").innerHTML = rLine;
+}
+
+function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=pHeight;i >= 0;i--){
+rLine +="<p>";
+//Create each line on the Rhombus
+for(k=0;k<(pHeight-i); k++) {
+  rLine +="<span style='color:#FFFFFF;'>" + pSymbol +"</span>";
+}
+for(j=0;j<i*2+1;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("downRight").innerHTML = rLine;
+}
+
+ 
